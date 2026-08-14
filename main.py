9c103f7b1f607943c1f1989ff42153ce1ec8c2f5
ArON <89467224+Aps2021@users.noreply.github.com>
2026-08-14 18:10:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,11 @@ def clima_rss():
             vento_graus = round(data['wind']['deg']) if data.get('wind') and 'deg' in data['wind'] else 0
             vento_direcao_texto = graus_para_cardeal(vento_graus)
 
-            title = f"{city} – {temp_atual}°C: {desc_clima}"
+            title = f"{city} – {temp_atual}°C / {desc_clima}"
             
             # ESTRUTURA ATUALIZADA: Direção do Vento inserida antes da Velocidade, separada por "; "
             desc = (
-                f"Temperatura: Mín: {temp_min}°C / Máx: {temp_max}°C; "
+                f"Temperatura: (Mín: {temp_min}°C / Máx: {temp_max}°C); "
                 f"Umidade: {umidade_atual}%; "
                 f"Vento: {vento_velocidade} km/h - "
                 f"Sentido: {vento_direcao_texto}; "
